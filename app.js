409db3aecbdfb45728d181b225b919e9a8a64643
DSHIVARAJ17 <161930848+DSHIVARAJ17@users.noreply.github.com>
2025-12-15 18:13:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.get("/", (req, res) => {
-    res.render("index");  
+  res.send("<h1>Hi Hello!</h1>");
 });
 
-app.listen(process.env.PORT||3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
